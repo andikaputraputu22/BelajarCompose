788342af0ba5b50d4034654525a3fd1ac3317d49
I Putu Andika Putra <andikaputraputu22@gmail.com>
2023-10-09 16:30:03 +0700

    WIP Image compose

--- app/src/main/java/com/moonlightsplitter/belajarcompose/MainActivity.kt
+++ app/src/main/java/com/moonlightsplitter/belajarcompose/MainActivity.kt
@@ -3,17 +3,25 @@ package com.moonlightsplitter.belajarcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.moonlightsplitter.belajarcompose.ui.theme.BelajarComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,32 +30,46 @@ class MainActivity : ComponentActivity() {
         setContent {
             BelajarComposeTheme {
                 // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-                Greeting("I Putu Andika Putra")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(message = "Happy Birthday Andika!", from = "from Ratih")
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Black) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
         Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp), color = Color.White
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(painter = image, contentDescription = null)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BelajarComposeTheme {
-        Greeting("I Putu Andika Putra")
+        GreetingImage(message = "Happy Birthday Sam!", from = "from Emma")
     }
 }
