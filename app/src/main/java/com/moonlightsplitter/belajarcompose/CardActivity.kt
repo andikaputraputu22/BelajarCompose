2d358a50884b6944ac387377a8eaa59fae8c4d58
I Putu Andika Putra <andikaputraputu22@gmail.com>
2023-10-11 01:56:22 +0700

    WIP Card page

---
+++ app/src/main/java/com/moonlightsplitter/belajarcompose/CardActivity.kt
@@ -0,0 +1,148 @@
+package com.moonlightsplitter.belajarcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.moonlightsplitter.belajarcompose.data.model.ProgrammerModel
+import com.moonlightsplitter.belajarcompose.ui.theme.BelajarComposeTheme
+
+class CardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BelajarComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val data = ProgrammerModel(
+                        "Andika Putra",
+                        "Android Programmer at Detikcom",
+                        "081339891936",
+                        "@andikaputraputu",
+                        "andikaputraputu22@gmail.com"
+                    )
+                    CardName(data = data)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CardName(data: ProgrammerModel) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+            .weight(6f)
+            .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            ContentName(data = data)
+        }
+        Column(modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .padding(bottom = 16.dp),
+            verticalArrangement = Arrangement.Bottom) {
+            Info(icon = Icons.Rounded.Phone, data = data.phone)
+            Info(icon = Icons.Rounded.Share, data = data.socialMedia)
+            Info(icon = Icons.Rounded.Email, data = data.email)
+        }
+//        ContentName(data = data)
+    }
+
+}
+
+@Composable
+fun ContentName(data: ProgrammerModel) {
+    val image = painterResource(R.drawable.android_logo)
+
+        Image(painter = image, contentDescription = null, Modifier.size(100.dp))
+        Text(
+            text = data.name,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = data.job
+        )
+
+}
+
+@Composable
+fun ContentInfo(data: ProgrammerModel) {
+    Box(
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Column {
+            Info(icon = Icons.Rounded.Phone, data = data.phone)
+            Info(icon = Icons.Rounded.Share, data = data.socialMedia)
+            Info(icon = Icons.Rounded.Email, data = data.email)
+        }
+    }
+}
+
+@Composable
+fun Info(icon: ImageVector, data: String) {
+    Row {
+        Icon(icon, contentDescription = null)
+        Text(
+            text = data,
+            modifier = Modifier
+                .padding(start = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    BelajarComposeTheme {
+        val data = ProgrammerModel(
+            "Andika Putra",
+            "Android Programmer at Detikcom",
+            "081339891936",
+            "@andikaputraputu",
+            "andikaputraputu22@gmail.com"
+        )
+        CardName(data = data)
+    }
+}
---
+++ app/src/main/java/com/moonlightsplitter/belajarcompose/data/model/ProgrammerModel.kt
@@ -0,0 +1,9 @@
+package com.moonlightsplitter.belajarcompose.data.model
+
+data class ProgrammerModel(
+    val name: String,
+    val job: String,
+    val phone: String,
+    val socialMedia: String,
+    val email: String
+)
